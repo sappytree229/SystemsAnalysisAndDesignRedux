e243e9d42b20e83446f7a347618e0a1abcafd011
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-11-09 12:51:29 -0600

    1st Overhaul Push

---
+++ .Net Overhaul/ISAD User Story 1/ABITS Form.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace ISAD_User_Story_1
+{
+    partial class aBITSForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fNameLabel = new System.Windows.Forms.Label();
+            this.lNameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.majorLabel = new System.Windows.Forms.Label();
+            this.fNameTextBox = new System.Windows.Forms.TextBox();
+            this.lNameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.yearOfStudyLabel = new System.Windows.Forms.Label();
+            this.yearOfStudyComboBox = new System.Windows.Forms.ComboBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.majorComboBox = new System.Windows.Forms.ComboBox();
+            this.sponsorCheckBox = new System.Windows.Forms.CheckBox();
+            this.aBITSDataSet = new ISAD_User_Story_1.ABITSDataSet();
+            this.prospective_Member_InfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.prospective_Member_InfoTableAdapter = new ISAD_User_Story_1.ABITSDataSetTableAdapters.Prospective_Member_InfoTableAdapter();
+            this.tableAdapterManager = new ISAD_User_Story_1.ABITSDataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBITSDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prospective_Member_InfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ISAD_User_Story_1.Properties.Resources.USB_Card_Front;
+            this.pictureBox1.Location = new System.Drawing.Point(108, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(679, 273);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fNameLabel
+            // 
+            this.fNameLabel.AutoSize = true;
+            this.fNameLabel.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameLabel.Location = new System.Drawing.Point(103, 326);
+            this.fNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.fNameLabel.Name = "fNameLabel";
+            this.fNameLabel.Size = new System.Drawing.Size(130, 25);
+            this.fNameLabel.TabIndex = 1;
+            this.fNameLabel.Text = "First Name:";
+            // 
+            // lNameLabel
+            // 
+            this.lNameLabel.AutoSize = true;
+            this.lNameLabel.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameLabel.Location = new System.Drawing.Point(103, 391);
+            this.lNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lNameLabel.Name = "lNameLabel";
+            this.lNameLabel.Size = new System.Drawing.Size(133, 25);
+            this.lNameLabel.TabIndex = 2;
+            this.lNameLabel.Text = "Last Name:";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.Location = new System.Drawing.Point(103, 456);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(77, 25);
+            this.emailLabel.TabIndex = 3;
+            this.emailLabel.Text = "Email:";
+            // 
+            // majorLabel
+            // 
+            this.majorLabel.AutoSize = true;
+            this.majorLabel.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.majorLabel.Location = new System.Drawing.Point(103, 519);
+            this.majorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.majorLabel.Name = "majorLabel";
+            this.majorLabel.Size = new System.Drawing.Size(81, 25);
+            this.majorLabel.TabIndex = 4;
+            this.majorLabel.Text = "Major:";
+            // 
+            // fNameTextBox
+            // 
+            this.fNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.prospective_Member_InfoBindingSource, "fName", true));
+            this.fNameTextBox.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fNameTextBox.Location = new System.Drawing.Point(400, 323);
+            this.fNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fNameTextBox.Name = "fNameTextBox";
+            this.fNameTextBox.Size = new System.Drawing.Size(385, 34);
+            this.fNameTextBox.TabIndex = 5;
+            this.fNameTextBox.TextChanged += new System.EventHandler(this.fNameTextBox_TextChanged);
+            // 
+            // lNameTextBox
+            // 
+            this.lNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.prospective_Member_InfoBindingSource, "lName", true));
+            this.lNameTextBox.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNameTextBox.Location = new System.Drawing.Point(400, 388);
+            this.lNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lNameTextBox.Name = "lNameTextBox";
+            this.lNameTextBox.Size = new System.Drawing.Size(385, 34);
+            this.lNameTextBox.TabIndex = 6;
+            this.lNameTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.prospective_Member_InfoBindingSource, "email", true));
+            this.emailTextBox.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTextBox.Location = new System.Drawing.Point(400, 453);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(385, 34);
+            this.emailTextBox.TabIndex = 7;
+            this.emailTextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // yearOfStudyLabel
+            // 
+            this.yearOfStudyLabel.AutoSize = true;
+            this.yearOfStudyLabel.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearOfStudyLabel.Location = new System.Drawing.Point(103, 582);
+            this.yearOfStudyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.yearOfStudyLabel.Name = "yearOfStudyLabel";
+            this.yearOfStudyLabel.Size = new System.Drawing.Size(161, 25);
+            this.yearOfStudyLabel.TabIndex = 9;
+            this.yearOfStudyLabel.Text = "Year of Study:";
+            // 
+            // yearOfStudyComboBox
+            // 
+            this.yearOfStudyComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.prospective_Member_InfoBindingSource, "studentStatus", true));
+            this.yearOfStudyComboBox.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearOfStudyComboBox.FormattingEnabled = true;
+            this.yearOfStudyComboBox.Items.AddRange(new object[] {
+            "Freshman",
+            "Sophomore",
+            "Junior",
+            "Senior"});
+            this.yearOfStudyComboBox.Location = new System.Drawing.Point(400, 579);
+            this.yearOfStudyComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.yearOfStudyComboBox.Name = "yearOfStudyComboBox";
+            this.yearOfStudyComboBox.Size = new System.Drawing.Size(385, 32);
+            this.yearOfStudyComboBox.TabIndex = 10;
+            this.yearOfStudyComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Constantia", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(350, 709);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(191, 68);
+            this.submitButton.TabIndex = 11;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // majorComboBox
+            // 
+            this.majorComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.prospective_Member_InfoBindingSource, "major", true));
+            this.majorComboBox.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.majorComboBox.FormattingEnabled = true;
+            this.majorComboBox.Items.AddRange(new object[] {
+            "Application Development",
+            "Cyber Security",
+            "Infrastructure",
+            "Other"});
+            this.majorComboBox.Location = new System.Drawing.Point(400, 516);
+            this.majorComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.majorComboBox.Name = "majorComboBox";
+            this.majorComboBox.Size = new System.Drawing.Size(385, 32);
+            this.majorComboBox.TabIndex = 12;
+            this.majorComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // sponsorCheckBox
+            // 
+            this.sponsorCheckBox.AutoSize = true;
+            this.sponsorCheckBox.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sponsorCheckBox.Location = new System.Drawing.Point(250, 652);
+            this.sponsorCheckBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sponsorCheckBox.Name = "sponsorCheckBox";
+            this.sponsorCheckBox.Size = new System.Drawing.Size(390, 28);
+            this.sponsorCheckBox.TabIndex = 13;
+            this.sponsorCheckBox.Text = " I am interested in sponsoring A-BITS!";
+            this.sponsorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // aBITSDataSet
+            // 
+            this.aBITSDataSet.DataSetName = "ABITSDataSet";
+            this.aBITSDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // prospective_Member_InfoBindingSource
+            // 
+            this.prospective_Member_InfoBindingSource.DataMember = "Prospective_Member_Info";
+            this.prospective_Member_InfoBindingSource.DataSource = this.aBITSDataSet;
+            // 
+            // prospective_Member_InfoTableAdapter
+            // 
+            this.prospective_Member_InfoTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.Prospective_Member_InfoTableAdapter = this.prospective_Member_InfoTableAdapter;
+            this.tableAdapterManager.UpdateOrder = ISAD_User_Story_1.ABITSDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // aBITSForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 791);
+            this.Controls.Add(this.sponsorCheckBox);
+            this.Controls.Add(this.majorComboBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.yearOfStudyComboBox);
+            this.Controls.Add(this.yearOfStudyLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.lNameTextBox);
+            this.Controls.Add(this.fNameTextBox);
+            this.Controls.Add(this.majorLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.lNameLabel);
+            this.Controls.Add(this.fNameLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "aBITSForm";
+            this.Text = "A-BITS Information Request";
+            this.Load += new System.EventHandler(this.aBITSForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aBITSDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prospective_Member_InfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label fNameLabel;
+        private System.Windows.Forms.Label lNameLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label majorLabel;
+        private System.Windows.Forms.TextBox fNameTextBox;
+        private System.Windows.Forms.TextBox lNameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label yearOfStudyLabel;
+        private System.Windows.Forms.ComboBox yearOfStudyComboBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.ComboBox majorComboBox;
+        private System.Windows.Forms.CheckBox sponsorCheckBox;
+        private ABITSDataSet aBITSDataSet;
+        private System.Windows.Forms.BindingSource prospective_Member_InfoBindingSource;
+        private ABITSDataSetTableAdapters.Prospective_Member_InfoTableAdapter prospective_Member_InfoTableAdapter;
+        private ABITSDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+    }
+}
+
---
+++ .Net Overhaul/ISAD User Story 1/ABITS Form.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISAD_User_Story_1
+{
+    public partial class aBITSForm : Form
+    {
+        public aBITSForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand query = new SqlCommand("Insert into PROSPECTIVE_MEMBER_INFO (fName) values (" + fNameTextBox.Text + ")");
+        }
+
+        private void aBITSForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'aBITSDataSet.Prospective_Member_Info' table. You can move, or remove it, as needed.
+            this.prospective_Member_InfoTableAdapter.Fill(this.aBITSDataSet.Prospective_Member_Info);
+
+        }
+
+        private void fNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
